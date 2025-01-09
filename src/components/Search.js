d0ac5309ae0215e21414b0af60a3407923083673
Ezi99 <ismaeljubranja@mta.ac.il>
2025-01-09 21:41:14 +0200

    Add: focus on search when pressing enter
    added while learning about useRef hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,4 +1,29 @@
+import { useEffect, useRef } from "react";
+
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(
+    function () {
+      function callback(event) {
+        if (document.activeElement === inputEl.current) {
+          return;
+        }
+
+        if (event.code === "Enter") {
+          console.log(inputEl.current);
+          inputEl.current.focus();
+          setQuery("");
+        }
+      }
+
+      document.addEventListener("keydown", callback);
+
+      return () => document.removeEventListener("keydown", callback);
+    },
+    [setQuery]
+  );
+
   return (
     <input
       className="search"
@@ -6,6 +31,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
